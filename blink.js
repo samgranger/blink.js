e60e3c9bb37c2a50db5a9788684b14ab1c831a39
Sam Granger <hello@samgranger.com>
2013-08-07 22:21:11 +0200

    Initial version

---
+++ blink.js
@@ -0,0 +1,33 @@
+var userAgentMatch = navigator.userAgent.match(/Firefox\/(.*)$/),
+    firefoxVersion;
+
+if (userAgentMatch && userAgentMatch.length > 1) {
+    firefoxVersion = userAgentMatch[1];
+}
+
+if (userAgentMatch && userAgentMatch.length > 1 && firefoxVersion >= 23) {
+    function blinkTags() {
+        var visible = true;
+
+        setInterval(function () {
+            var blinks = document.getElementsByTagName('blink');
+
+            if (visible == false) {
+                for (var i = 0; i < blinks.length; i++) {
+                    blinks[i].style.visibility = "visible";
+                }
+                visible = true;
+            } else {
+                for (var i = 0; i < blinks.length; i++) {
+                    blinks[i].style.visibility = "hidden";
+                }
+                visible = false;
+            }
+        }, 500);
+    }
+    
+    window.onload = function () {
+    	alert(navigator.userAgent);
+        blinkTags();
+    }
+}
