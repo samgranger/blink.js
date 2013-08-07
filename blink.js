bb8ea057fa44030d793127687c744a3f8c01d483
Sam Granger <hello@samgranger.com>
2013-08-07 22:33:20 +0200

    Cleaning up code

--- blink.js
+++ blink.js
@@ -9,25 +9,25 @@ if (userAgentMatch && userAgentMatch.length > 1 && firefoxVersion >= 23) {
     function blinkTags() {
         var visible = true;
 
-        setInterval(function () {
+        setInterval(function() {
             var blinks = document.getElementsByTagName('blink');
 
             if (visible == false) {
                 for (var i = 0; i < blinks.length; i++) {
-                    blinks[i].style.visibility = "visible";
+                    blinks[i].style.visibility = 'visible';
                 }
                 visible = true;
             } else {
                 for (var i = 0; i < blinks.length; i++) {
-                    blinks[i].style.visibility = "hidden";
+                    blinks[i].style.visibility = 'hidden';
                 }
                 visible = false;
             }
         }, 500);
     }
-    
-    window.onload = function () {
+
+    window.onload = function() {
     	alert(navigator.userAgent);
         blinkTags();
-    }
+    };
 }
